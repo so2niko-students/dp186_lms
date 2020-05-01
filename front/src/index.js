d577afa51b8a5cd69802655f952bb9ae33e25348
staspitsyk <staspitsyk@gmail.com>
2020-05-01 14:06:18 +0300

    Added app component

--- front/src/index.js
+++ front/src/index.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import App from './components/app';
 import store from './common/redux/store';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import StudentRegistrationPage from './pages/student-registration-page';
 
-class App extends Component {
-    render() {
-        return (
-          <Provider store={store}>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/student-registration/:token" component={StudentRegistrationPage} />
-                </Switch>
-            </BrowserRouter>
-          </Provider>
-        )
-    }
-}
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-render(<App />, document.getElementById('root'));
+render(app, document.getElementById('root'));
