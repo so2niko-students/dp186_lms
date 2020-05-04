e352ad9bfcecf22ff08052fa78029fe8049b7fc3
Kateryna <maslova.ekaterina94@gmail.com>
2020-05-04 19:15:16 +0300

    set user to localStorage in auth.saga, some fixes ListOfGroup

--- front/src/common/redux/groups/groups.reducer.js
+++ front/src/common/redux/groups/groups.reducer.js
@@ -1,13 +1,13 @@
 import * as types from './types';
 
 const initialState = {
-  currentGroup: 1,
+  currentGroup: {},
 };
 
 export function groupList(state = initialState, action) {
   switch (action.type) {
     case types.SET_CURRENT_GROUP:
-      return { ...state, currentGroup: +action.data.key };
+      return { ...state, currentGroup: action.data };
     default:
       return { ...state };
   }
--- front/src/containers/list-of-group/index.js
+++ front/src/containers/list-of-group/index.js
@@ -1,41 +1,63 @@
 import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
+import axios from 'axios';
 import 'antd/dist/antd.css';
 import { AddGroupButton, SiderStyle } from './style';
 import { setCurrentGroup } from '../../common/redux/groups/groups.action';
 import { connect } from 'react-redux';
 
-const { Content } = Layout;
+let groupList;
 
 class ListOfGroup extends Component {
-    componentDidMount() {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            groupList: null
+        }
     }
+
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+        const userId = JSON.parse(localStorage.getItem('user')).id;
+        const url = `${process.env.REACT_APP_GROUPS_BY_MENTOR}${userId}`;
+        const headers = { headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`, } };
+
+        axios.get(url, headers)
+            .then(response => {
+                this.setState({ groupList: response.data })
+                groupList = response.data; // for dispatch 
+                this.props.onHandleFirstGroup(groupList[0])
+            })
+    }
+
     render() {
-        console.log(this.props)
+        const { onHandleGroup } = this.props;
+        const { groupList } = this.state;
         return (
             <Layout>
                 <SiderStyle width={200} className="site-layout-background" >
                     <AddGroupButton type="primary"> Add group </AddGroupButton>
                     <Menu style={{ overflow: 'auto', position: 'fixed', left: 0, height: '90%', borderRight: 0, width: 200 }} mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} >
-                        {this.props.groups.map(group => <Menu.Item key={group.id} onClick={this.props.onHandleGroup}>{group.name}</Menu.Item>)}
+                        {groupList ?
+                            groupList.map(group => <Menu.Item key={group.id} onClick={onHandleGroup}>{group.groupName}</Menu.Item>)
+                            : null}
                     </Menu>
                 </SiderStyle>
-                <Layout style={{ padding: '0 24px 24px' }}>
-                    <Content className="site-layout-background" style={{ padding: 24, margin: 0, minHeight: 280, }} >
-                        {this.props.currentGroup}
-                    </Content>
-                </Layout>
             </Layout>
         )
     }
 }
 
-const mapStateToProps = ({ groupList: { currentGroup } }) => ({ currentGroup });
+const mapStateToProps = ({ groupList: { currentGroup }, login: { userId } }) => ({ currentGroup, userId });
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onHandleGroup: (data) => dispatch(setCurrentGroup(data))
+        onHandleGroup: (data) => {
+            return dispatch(setCurrentGroup(groupList.find((group) => group.id === +data.key)))
+        },
+        onHandleFirstGroup: (data) => {
+            return dispatch(setCurrentGroup(data))
+        },
     }
 };
 
