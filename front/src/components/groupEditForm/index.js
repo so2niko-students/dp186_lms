423c65fab04f668c95d7c0b258672354fe19c145
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-05-06 16:36:02 +0300

    create edit form

--- front/src/components/groupEditForm/index.js
+++ front/src/components/groupEditForm/index.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
 import {Form, Input, Button, Upload } from 'antd';
 import { validateGroup } from '../../common/validators/group.validator';
+import LoadingOutlined from '@ant-design/icons/es/icons/LoadingOutlined';
+import PlusOutlined from '@ant-design/icons/es/icons/PlusOutlined';
 
 class GroupEditForm extends Component {
     state = {
         groupName: this.props.groupName,
+        file: null,
+        loading: false,
     }
 
-    handleUpdateGroup = (data) =>{
+    handleUpdateGroup = (data) => {
         console.log(data)
     }
 
+    handleSelectImg = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            this.setState({file: [reader.result]});
+        }
+        return false;
+    }
+
+    handleDeleteImg = (file) => {
+        this.setState({file: null});
+    }
+
     render() {
-        const { groupName } = this.state;
+        const { groupName, file } = this.state;
+        const uploadButton = (
+            <div>
+                {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
+                <div className="ant-upload-text">Upload</div>
+            </div>
+        );
+
         return (
             <div>
                 <Form
@@ -22,12 +46,24 @@ class GroupEditForm extends Component {
                     onFinish={this.handleUpdateGroup}
                 >
                     <Form.Item
+                        label={'Group avatar'}
                         name={'groupAvatar'}
                     >
-
+                        <Upload
+                            name="avatar"
+                            listType="picture-card"
+                            className="avatar-uploader"
+                            accept={'.jpg, .jpeg, .png'}
+                            showUploadList={false}
+                            fileList={false}
+                            onRemove={this.handleDeleteImg}
+                            beforeUpload={this.handleSelectImg}
+                        >
+                            {file ? <img src={file} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                        </Upload>
                     </Form.Item>
                     <Form.Item
-                        label={'Group Name'}
+                        label={'Group name'}
                         name={'groupName'}
                         rules={[
                             {required: true, message: 'Please enter group name'},
