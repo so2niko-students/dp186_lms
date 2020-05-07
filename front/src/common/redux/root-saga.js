3b460ea3fcd8c074c86fb5ffa0c0e6c04d01ae9a
staspitsyk <staspitsyk@gmail.com>
2020-05-07 16:17:42 +0300

    Fixed missing semicolons and spaces

--- front/src/common/redux/root-saga.js
+++ front/src/common/redux/root-saga.js
@@ -1,7 +1,7 @@
 import { fork } from 'redux-saga/effects';
-import {studentsSaga} from './students/students.saga';
+import { studentsSaga } from './students/students.saga';
 import { authSaga } from './auth/auth.saga';
-import {teachersSaga} from './teachers/teachers.saga';
+import { teachersSaga } from './teachers/teachers.saga';
 
 export default function* startForman() {
   yield fork(studentsSaga);
--- front/src/common/redux/teachers/teachers.actions.js
+++ front/src/common/redux/teachers/teachers.actions.js
@@ -1,9 +1,14 @@
-import { SHOW_TEACHER_REGISTER_MODAL, HIDE_TEACHER_REGISTER_MODAL, REGISTER_TEACHER, SHOW_LOADER } from './types';
+import {
+  SHOW_TEACHER_REGISTER_MODAL,
+  HIDE_TEACHER_REGISTER_MODAL,
+  REGISTER_TEACHER,
+  SHOW_LOADER,
+} from './types';
 
-export const registerTeacher = (teacher) => ({type: REGISTER_TEACHER,payload: teacher});
+export const registerTeacher = (teacher) => ({ type: REGISTER_TEACHER, payload: teacher });
 
-export const hideTeacherRegisteredModal = () => ({type: HIDE_TEACHER_REGISTER_MODAL})
+export const hideTeacherRegisteredModal = () => ({ type: HIDE_TEACHER_REGISTER_MODAL });
 
-export const showTeacherRegisteredModal = () => ( {type: SHOW_TEACHER_REGISTER_MODAL})
+export const showTeacherRegisteredModal = () => ({ type: SHOW_TEACHER_REGISTER_MODAL });
 
-export const showLoader = () => ({type: SHOW_LOADER})
+export const showLoader = () => ({ type: SHOW_LOADER });
