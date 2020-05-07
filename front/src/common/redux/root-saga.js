9871dbcfff44c7aa4a7800bdb98e66f5a0a4f12e
OksanaKozhukh <oksana.kozhukh71090@gmail.com>
2020-05-07 23:50:20 +0300

    Revert "add saga"
    This reverts commit b7120b2f91c851714aca59722bf54b1c9febef43.

--- front/src/common/redux/root-saga.js
+++ front/src/common/redux/root-saga.js
@@ -1,10 +1,8 @@
 import { fork } from 'redux-saga/effects';
 import { studentsSaga } from './students/students.saga';
 import { authSaga } from './auth/auth.saga';
-import { passwordSaga } from './set-password/set.password.saga';
 
 export default function* startForman() {
   yield fork(studentsSaga);
   yield fork(authSaga);
-  yield fork(passwordSaga);
 }
