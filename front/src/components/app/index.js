4b5ff1c7b60213d22713027c2cc35c2b38b440a1
staspitsyk <staspitsyk@gmail.com>
2020-05-09 13:07:53 +0300

    Removed connet from App component

--- front/src/components/app/index.js
+++ front/src/components/app/index.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import StudentRegistrationPage from '../../pages/student-registration-page';
 import TeachersPage from '../../pages/teachers/teachers-page';
-import {showTeacherRegisteredModal} from '../../common/redux/teachers/teachers.actions'
-import { connect } from 'react-redux';
 import LoginPage from '../../pages/login';
 
 class App extends Component {
@@ -13,11 +11,6 @@ class App extends Component {
                 <Switch>
                     <Route path="/student-registration/:token" component={StudentRegistrationPage} />
                     <Route path="/teachers" exact component={TeachersPage} />
-                    <Route path="/teachers/create"
-                       render={() => {
-                        this.props.showTeacherRegisteredModal();
-                         return <TeachersPage />
-                       }}/>
                     <Route path="/login" component={LoginPage} />
                 </Switch>
             </BrowserRouter>
@@ -25,10 +18,4 @@ class App extends Component {
     }
 }
 
-
-  
-const mapDispatchToProps = {
-    showTeacherRegisteredModal
-};
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
