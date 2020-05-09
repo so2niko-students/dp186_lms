5f6a75ff256d7724d956b4371cc2c7c3bae14e34
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-05-10 01:11:19 +0300

    style: fix styles

--- front/src/components/group/style.js
+++ front/src/components/group/style.js
@@ -16,6 +16,12 @@ export const GroupTitleP = style.p`
 export const StyledColHeader = style(Col)`
     position: relative;
     border-bottom: 1px solid #cccccc;
+    min-height: 190px;
+    min-width: 360px;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const EditGroupBtn = style(Button)`
@@ -29,35 +35,53 @@ export const StyledUserBlock = style(Col)`
 `;
 
 export const StyledStudentBlock = style(StyledUserBlock)`
-    background: #f1f1f1;
+    background: #ffffff;
     padding: 20px 0;
     border-radius: 10px;
 `;
 
 export const StyledTeachersBlock = style(StyledUserBlock)`
-    
+    @media screen and (max-width: 576px) {
+        margin-top: 25px;
+    }
 `;
 
 export const StyledList = style(List)`
     max-height: 550px;
     overflow-y: scroll;
+    @media screen and (max-width: 576px) {
+        max-height: 100%;
+        overflow-y: auto;
+    }
 `;
 
 export const StyledListItem = style(List.Item)`
-    justify-content: flex-start;
+    justify-content: space-between;
     margin: 0 30px;
     border-bottom: 1px solid #cccccc!important;
     &:last-child {
         border-bottom: 0px!important;
     }
+    @media screen and (max-width: 576px) {
+        flex-flow: column;
+    }
 `;
 
 export const StyledListItemMeta = style(List.Item.Meta)`
     align-items: center;
-    padding: 15px 55px;
+    padding: 15px;
     flex: none;
     &>div>h4 {
         text-align: left;
+    }
+    @media screen and (max-width: 576px) {
+        flex-flow: column;
+        &>div.ant-list-item-meta-avatar {
+            margin-right: 0px;
+        }
+        &>div>h4 {
+            text-align: center;
+        }
     }
 `;
 
@@ -78,7 +102,9 @@ export const StyledP = style.p`
     margin: -10px 0 0 0!important;
     text-align: left;
     color: #adadad;
-    
+    @media screen and (max-width: 576px) {
+        text-align: center;
+    }
 `;
 
 export const StyledDivBtn = style.div`
