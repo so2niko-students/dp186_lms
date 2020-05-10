187bdc4b0666fd286d57e07ac50fb14d043029d8
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-05-11 01:12:21 +0300

    feat(ReduxDevToolExtension): install redux-devtools-extension package

--- front/src/common/redux/store.js
+++ front/src/common/redux/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './root-reducer';
 import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  rootReducer,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(sagaMiddleware)
+  rootReducer, composeWithDevTools(
+        applyMiddleware(sagaMiddleware)
+    )
 );
 sagaMiddleware.run(rootSaga);
 
