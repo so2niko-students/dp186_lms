412b713dc22f1fb4b5f98fb91aae2ae3d48d80cf
Kateryna <maslova.ekaterina94@gmail.com>
2020-05-11 13:20:59 +0300

    merge

--- front/src/components/app/index.js
+++ front/src/components/app/index.js
@@ -3,11 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StudentRegistrationPage from '../../pages/student-registration-page';
 import TeachersPage from '../../pages/teachers/teachers-page';
 import LoginPage from '../../pages/login';
-import TasksList from '../../pages/tasks-list';
 import GroupsPage from '../../pages/groups';
 import SetPasswordPage from '../../pages/set-password';
 import ForgotPasswordPage from '../../pages/forgot-password';
-
+import TasksList from '../../pages/tasks-list';
 
 class App extends Component {
     render() {
@@ -17,10 +16,10 @@ class App extends Component {
                     <Route path="/student-registration/:token" component={StudentRegistrationPage} />
                     <Route path="/teachers" exact component={TeachersPage} />
                     <Route path="/login" component={LoginPage} />
-                    <Route path="/tasks" component={TasksList} />
                     <Route path="/resetPassword/:token" component={SetPasswordPage} />
                     <Route path="/forgotPassword/" component={ForgotPasswordPage} />
                     <Route path="/groups" component={GroupsPage} />
+                    <Route path="/tasks" component={TasksList} />
                 </Switch>
             </BrowserRouter>
         )
