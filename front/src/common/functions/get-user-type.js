4cdabb486a5f72b7f59172b3135f1b3c7659c8c0
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-05-12 00:20:21 +0300

    fix: dispatch in group-list component and get-user-type function

--- front/src/common/functions/get-user-type.js
+++ front/src/common/functions/get-user-type.js
@@ -1,10 +1,20 @@
 export const STUDENT = 'student';
 export const SUPERADMIN = 'superAdmin';
 export const MENTOR = 'mentor';
+export const UNAUTHORIZED = 'unauthorized';
 
 const getUserType = () => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if(user.isAdmin !== undefined){
+    // if we have user key without data in local storage
+    if (!localStorage.getItem('user') ){
+        return UNAUTHORIZED;
+    }
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    // if we dont have user key in local storage
+    if (!user.email) {
+        return UNAUTHORIZED;
+    }
+    // if we have user key with data in local storage
+    if (user.isAdmin !== undefined) {
         return user.isAdmin ? SUPERADMIN : MENTOR;
     }
     return STUDENT;
